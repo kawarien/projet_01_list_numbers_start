e8f0cd52b00ea188ffeb2c7e259326f7170ada4e
ABBA ADAM Ibrahim <ibrahim.abbaadam@francetv.fr>
2020-12-09 17:20:44 +0100

    permit numbers for methode

--- src/app.js
+++ src/app.js
@@ -8,23 +8,52 @@ console.log(els)
 
 
 
-console.log(' --  Avant la permutation --- ')
+// console.log(' --  Avant la permutation --- ')
 
-let myTab = Array.from(document.querySelectorAll('li'))
-console.log('myTab', myTab)
+// let myTab = Array.from(document.querySelectorAll('li'))
+// console.log('myTab', myTab)
 
-let [l1,l2,l3] = myTab
+// let [l1,l2,l3] = myTab
 
-const ul = document.querySelector('ul.numbers');
+// const ul = document.querySelector('ul.numbers');
 
-[l3, l1, l2].map(lesLi   => {
-  console.log(' --  Apres la permutation --- ', lesLi);
-  ul.appendChild(l1);
+// [l3, l1, l2].map(lesLi   => {
+//   console.log(' --  Apres la permutation --- ', lesLi);
+//   ul.appendChild(l1);
+// }
+// )
+
+
+
+
+// Boucles JS
+
+function perm(xs) {
+  let mysNewTab = [];
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if(!rest.length) {
+      mysNewTab.push([xs[i]])
+    } else {
+      for(let j = 0; j < rest.length; j = j + 1) {
+        mysNewTab.push([xs[i]].concat(rest[j]))
+      }
+    }
+  }
+  return mysNewTab;
 }
-)
+ let myTab = Array.from(document.querySelectorAll('li'))
+ console.log(myTab)
+console.log(perm(myTab).join("\n"));
 
+//  let myTab = Array.from(document.querySelectorAll('li'))
+// for(let i = 0; i < myTab.length; i++){
+//   console.log(i);
+// }
 
-
+// // Calculer la longueur d'un tableau
+// console.log('longueur du tab ',myTab.length)
 
 
 // let  text = Array.from(document.querySelectorAll('span'))
