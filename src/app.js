f05355c8177a49b47f3242c1dd724b9c49c8cc73
ABBA ADAM Ibrahim <ibrahim.abbaadam@francetv.fr>
2020-12-09 15:11:28 +0100

    permit numbers ok

--- src/app.js
+++ src/app.js
@@ -2,12 +2,39 @@ import parser from './core/utils';
 
 
 
-const main = document.getElementById("main");
+
+let els = document.querySelectorAll('ul.numbers > li');
+console.log(els)
+
+
+
+console.log(' --  Avant la permutation --- ')
+
+let myTab = Array.from(document.querySelectorAll('li'))
+console.log('myTab', myTab)
+
+let [l1,l2,l3] = myTab
+
+const ul = document.querySelector('ul.numbers');
+
+[l3, l1, l2].map(lesLi   => {
+  console.log(' --  Apres la permutation --- ', lesLi);
+  ul.appendChild(l1);
+}
+)
+
+
+
+
+
+// let  text = Array.from(document.querySelectorAll('span'))
+//   .map(el => el.innerText);
+// console.log("avec map ",text) 
+
 
 // On attend que le DOM soit construit avant d'exécuter nos fonctions
 document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("App.js")
-  console.log(parser("Bonjour tout le monde"));
+
 
 
 });
--- src/core/utils.js
+++ src/core/utils.js
@@ -1,5 +1,1 @@
 
-export default  (str) => {
-
-  console.log(str.split(''));
-}
